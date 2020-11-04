be2b9e5419fb5e6762c3dccdd76f3fd625a608d2
Yan-Rong Li (Dell Laptop) <liyanrong@mail.ihep.ac.cn>
2020-11-04 09:53:54 +0800

    fix a bug in get_num_particles.

--- src/read.c
+++ src/read.c
@@ -513,23 +513,36 @@ void scale_con_line()
 void get_num_particles(char *fname)
 {
   FILE *fp;
-  char buf[MICA_MAX_STR_LENGTH], buf1[MICA_MAX_STR_LENGTH];
+  char buf[MICA_MAX_STR_LENGTH], buf1[MICA_MAX_STR_LENGTH], buf2[MICA_MAX_STR_LENGTH];
   fp = fopen(fname, "r");
   if(fp == NULL)
   {
     fprintf(stderr, "# Error: Cannot open file %s\n", fname);
     exit(-1);
   }
+  
+  /* default number particles */
+  parset.num_particles = 1;
 
-  buf[0]='#';
-  while(buf[0]=='#')
+  while(!feof(fp))
   {
     fgets(buf, MICA_MAX_STR_LENGTH, fp);
-    if(sscanf(buf, "%s", buf1) < 1)  // a blank line
+    if(buf[0] == '#')
+      continue;
+    if(sscanf(buf, "%s%s", buf1, buf2) < 1)  /* a blank line */
+      continue;
+    if(sscanf(buf, "%s%s", buf1, buf2) < 2)
     {
-      buf[0] = '#';
+      fprintf(stderr, "Error in geting number of particles.\n"
+                      "Usually due to incorrect options.\n");
+      exit(0);
+    }
+    if(strcmp(buf1, "NumberParticles") == 0)
+    {
+      parset.num_particles = atoi(buf2);
+      break;
     }
   }
-  sscanf(buf, "%d", &parset.num_particles);
+
   fclose(fp);
 }

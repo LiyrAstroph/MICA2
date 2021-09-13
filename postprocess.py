6c55a76eec2d75d31aacf723b4c61da14edf3d41
Yan-Rong Li <liyanrong@mail.ihep.ac.cn>
2021-09-13 13:27:05 +0800

    update postprocess.py.

--- postprocess.py
+++ postprocess.py
@@ -58,34 +58,31 @@ def postprocess(fdir, ngau, temperature=1.0):
   idx = (sample_info[:, 0] > levels_orig.shape[0] - 1)
   sample_info[idx, 0] = levels_orig.shape[0] - 1
   
-  pdf = PdfPages(fdir+'/data/dnest_%d.pdf'%ngau)
+  pdf = PdfPages(fdir+'/data/cdnest_%d.pdf'%ngau)
 
-  plt.figure(1)
-  plt.plot(sample_info[:,0], "k")
-  plt.xlabel("Iteration")
-  plt.ylabel("Level")
+  fig = plt.figure(figsize=(12, 8))
+  fig.subplots_adjust(wspace=0.2, hspace=0.3)
+  ax1 = fig.add_subplot(3, 1, 1)
+  ax1.plot(sample_info[:,0], "k", lw=1)
+  ax1.set_xlabel("Iteration")
+  ax1.set_ylabel("Level")
   
-  pdf.savefig()
-  
-  plt.figure(2)
-  plt.subplot(2,1,1)
-  plt.plot(np.diff(levels_orig[:,0]), "k")
-  plt.ylabel("Compression")
-  plt.xlabel("Level")
+  ax=fig.add_subplot(3,2,3)
+  ax.plot(np.diff(levels_orig[:,0]), "k")
+  ax.set_ylabel("Compression")
+  ax.set_xlabel("Level")
   xlim = plt.gca().get_xlim()
-  plt.axhline(-1., color='g')
-  plt.axhline(-np.log(10.), color='g', linestyle="--")
-  plt.ylim(ymax=0.05)
+  ax.axhline(-1., color='g')
+  ax.axhline(-np.log(10.), color='g', linestyle="--")
+  ax.set_ylim(ymax=0.05)
   
-  plt.subplot(2,1,2)
+  ax=fig.add_subplot(3,2,5)
   good = np.nonzero(levels_orig[:,4] > 0)[0]
-  plt.plot(levels_orig[good,3]/levels_orig[good,4], marker='o')
-  plt.xlim(xlim)
-  plt.ylim([0., 1.])
-  plt.xlabel("Level")
-  plt.ylabel("MH Acceptance")
-      
-  pdf.savefig()
+  ax.plot(levels_orig[good,3]/levels_orig[good,4], marker='o')
+  ax.set_xlim(xlim)
+  ax.set_ylim([0., 1.])
+  ax.set_xlabel("Level")
+  ax.set_ylabel("MH Acceptance")
   
   logl_levels = [(levels_orig[i,1], levels_orig[i, 2]) for i in range(0, levels_orig.shape[0])]
   logl_samples = [(sample_info[i, 1], sample_info[i, 2], i) for i in range(0, sample.shape[0])]
@@ -159,16 +156,15 @@ def postprocess(fdir, ngau, temperature=1.0):
     P_samples[:,z] = np.exp(logP_samples[:,z])
     H_estimates[z] = -logz_estimates[z] + np.sum(P_samples[:,z]*logl)
   
-    plt.figure(3)
-    plt.subplot(2,1,1)
-    #plt.hold(False)
-    plt.plot(logx_samples[:,z], sample_info[:,1], 'b.', label='Samples')
-    #plt.hold(True)
-    plt.plot(levels[1:,0], levels[1:,1], 'r.', label='Levels')
-    plt.legend(numpoints=1, loc='lower left')
-    plt.ylabel('log(L)')
-    plt.title(str(z+1) + "/" + str(numResampleLogX) + ", log(Z) = " + str(logz_estimates[z][0]))
-        # Use all plotted logl values to set ylim
+    ax=fig.add_subplot(3,2,4)
+    ax.plot(logx_samples[:,z], sample_info[:,1], 'b.', label='Samples')
+    ax.plot(levels[1:,0], levels[1:,1], 'r.', label='Levels')
+    ax.legend(numpoints=1, loc='lower left')
+    ax.set_ylabel('log(L)')
+    ax.set_xlabel('log(X)')
+    ax1.set_title(str(z+1) + "/" + str(numResampleLogX) + ", log(Z) = " + str(logz_estimates[z][0]))
+
+    # Use all plotted logl values to set ylim
     combined_logl = np.hstack([sample_info[:,1], levels[1:, 1]])
     combined_logl = np.sort(combined_logl)
     lower = combined_logl[int(0.1*combined_logl.size)]
@@ -177,19 +173,23 @@ def postprocess(fdir, ngau, temperature=1.0):
     lower -= 0.05*diff
     upper += 0.05*diff
     if zoom_in:
-      plt.ylim([lower, upper])
+      ax.set_ylim([lower, upper])
   
-    xlim = plt.gca().get_xlim()
-  
-    plt.subplot(2,1,2)
-    #plt.hold(False)
-    plt.plot(logx_samples[:,z], P_samples[:,z], 'b.')
-    plt.ylabel('Posterior Weights')
-    plt.xlabel('log(X)')
-    plt.xlim(xlim)
-    ylim = plt.gca().get_ylim()
-    plt.text(xlim[0]+0.1*(xlim[1]-xlim[0]), ylim[1]-0.1*(ylim[1]-ylim[0]), 'T='+str(temperature))
+    xlim = ax.get_xlim()
+
+    ax=fig.add_subplot(3,2,6)
+    ax.plot(logx_samples[:,z], P_samples[:,z], 'b.')
+    ax.set_ylabel('Posterior Weights')
+    ax.set_xlabel('log(X)')
+    ax.set_xlim(xlim)
+    
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    ax.text(xlim[0]+0.1*(xlim[1]-xlim[0]), ylim[1]-0.1*(ylim[1]-ylim[0]), 'T='+str(temperature))
+
+    fig.align_ylabels()
     pdf.savefig()
+    plt.close()
   
   pdf.close()
   P_samples = np.mean(P_samples, 1)

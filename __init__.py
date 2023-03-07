75803a0acc4dbf5696bfab2f02264466c193d68c
Yan-Rong Li <liyanrong@mail.ihep.ac.cn>
2023-03-07 17:25:40 +0800

    add __init__.py.

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+from .postprocess import *
+from .plotfig import *
+from .plotdecomp import *
+
+__all__ = ["plot_results", "postprocess", "plot_line_decomp"]
